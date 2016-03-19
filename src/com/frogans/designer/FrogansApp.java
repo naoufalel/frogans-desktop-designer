f21776e6ff10d3f6606c47a0f46bc8effe999ea3
naoufalel <naoufal477@gmail.com>
2016-03-19 15:12:37 +0100

    Create the main class for this project... extends javafx Application

---
+++ src/com/frogans/designer/FrogansApp.java
@@ -0,0 +1,32 @@
+package com.frogans.designer;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Main class that handles all the views and displays the app and starts plugins
+ *
+ * Created by Naoufal EL BANTLI on 3/19/2016.
+ */
+public class FrogansApp extends Application {
+
+    /**
+     * The main entry point for all JavaFX applications.
+     * The start method is called after the init method has returned,
+     * and after the system is ready for the application to begin running.
+     * <p>
+     * <p>
+     * NOTE: This method is called on the JavaFX Application Thread.
+     * </p>
+     *
+     * @param primaryStage the primary stage for this application, onto which
+     *                     the application scene can be set. The primary stage will be embedded in
+     *                     the browser if the application was launched as an applet.
+     *                     Applications may create other stages, if needed, but they will not be
+     *                     primary stages and will not be embedded in the browser.
+     */
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
