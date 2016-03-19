430a9df494555e06c12a7bd8b1f5a7cf6f5d0f5c
naoufalel <naoufal477@gmail.com>
2016-03-19 22:21:33 +0100

    added animations to TitledPanes and added controller for the designer layout

---
+++ src/com/frogans/designer/view/DesignerLayoutController.java
@@ -0,0 +1,32 @@
+package com.frogans.designer.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Accordion;
+import javafx.scene.control.TitledPane;
+
+/**
+ * Created by Naoufal EL BANTLI on 3/19/2016.
+ */
+public class DesignerLayoutController {
+
+    @FXML
+    private Accordion accordion;
+    @FXML
+    private TitledPane titledPane;
+
+
+
+
+    public DesignerLayoutController(){
+
+    }
+
+    @FXML
+    public void initialize(){
+        accordion.setExpandedPane(titledPane);
+    }
+
+
+
+
+}
--- src/com/frogans/designer/view/RootLayoutController.java
+++ src/com/frogans/designer/view/RootLayoutController.java
@@ -4,7 +4,7 @@ import com.frogans.designer.FrogansApp;
 import javafx.fxml.FXML;
 
 /**
- * Created by naouf on 3/19/2016.
+ * Created by Naoufal EL BANTLI on 3/19/2016.
  */
 public class RootLayoutController {
     private FrogansApp frogansApp;
@@ -21,4 +21,6 @@ public class RootLayoutController {
     public void initialize(){
 
     }
+
+
 }
