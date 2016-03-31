773d5c025eed735c5345eb5c2a51fd0a01d40eea
naoufalel <naoufal477@gmail.com>
2016-03-31 15:26:48 +0200

    button layout bitch

---
+++ src/com/frogans/designer/view/PropertiesLayout/ButtonLayoutController.java
@@ -0,0 +1,18 @@
+package com.frogans.designer.view.PropertiesLayout;
+
+import javafx.fxml.FXML;
+
+/**
+ * Created by naouf on 3/31/2016.
+ */
+public class ButtonLayoutController {
+
+
+
+    @FXML
+    public void initialize() {
+
+    }
+
+
+}
