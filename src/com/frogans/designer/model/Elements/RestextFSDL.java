e800eef96adbec01454a3917786b7bb942e75a4a
naoufalel <naoufal477@gmail.com>
2016-03-31 13:20:40 +0200

    doing things the right way again

--- src/com/frogans/designer/model/Elements/RestextFSDL.java
+++ src/com/frogans/designer/model/Elements/RestextFSDL.java
@@ -1,4 +1,4 @@
-package com.frogans.designer.model;
+package com.frogans.designer.model.Elements;
 
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
@@ -40,5 +40,112 @@ public class RestextFSDL {
             new SimpleStringProperty("upright"),
     };
 
+    public RestextFSDL() {
+        this.resid = new SimpleStringProperty("");
+        this.size = new SimpleStringProperty("");
+        this.fontref = new SimpleStringProperty("");
+        this.talign = new SimpleStringProperty("");
+        this.linespace = new SimpleStringProperty("");
+        this.vstyle = new SimpleStringProperty("");
+        this.orientation = new SimpleStringProperty("");
+    }
+    public RestextFSDL(StringProperty resid,
+                       StringProperty size,
+                       StringProperty fontref,
+                       StringProperty talign,
+                       StringProperty linespace,
+                       StringProperty vstyle,
+                       StringProperty orientation) {
+        this.resid = resid;
+        this.size = size;
+        this.fontref = fontref;
+        this.talign = talign;
+        this.linespace = linespace;
+        this.vstyle = vstyle;
+        this.orientation = orientation;
+    }
 
+    public String getResid() {
+        return resid.get();
+    }
+
+    public StringProperty residProperty() {
+        return resid;
+    }
+
+    public String getSize() {
+        return size.get();
+    }
+
+    public StringProperty sizeProperty() {
+        return size;
+    }
+
+    public String getOrientation() {
+        return orientation.get();
+    }
+
+    public StringProperty orientationProperty() {
+        return orientation;
+    }
+
+    public String getFontref() {
+        return fontref.get();
+    }
+
+    public StringProperty fontrefProperty() {
+        return fontref;
+    }
+
+    public String getTalign() {
+        return talign.get();
+    }
+
+    public StringProperty talignProperty() {
+        return talign;
+    }
+
+    public String getLinespace() {
+        return linespace.get();
+    }
+
+    public StringProperty linespaceProperty() {
+        return linespace;
+    }
+
+    public String getVstyle() {
+        return vstyle.get();
+    }
+
+    public StringProperty vstyleProperty() {
+        return vstyle;
+    }
+
+    public void setresid(String resid) {
+        this.resid.set(resid);
+    }
+
+    public void setsize(String size) {
+        this.size.set(size);
+    }
+
+    public void setorientation(String orientation) {
+        this.orientation.set(orientation);
+    }
+
+    public void setfontref(String fontref) {
+        this.fontref.set(fontref);
+    }
+
+    public void settalign(String talign) {
+        this.talign.set(talign);
+    }
+
+    public void setlinespace(String linespace) {
+        this.linespace.set(linespace);
+    }
+
+    public void setvstyle(String vstyle) {
+        this.vstyle.set(vstyle);
+    }
 }
