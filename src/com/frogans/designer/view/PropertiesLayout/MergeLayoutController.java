0910552bd19b89420bbb796a712e4aa3af691ae2
Abdelkabir ABOUZAHIR <ab.dabell@gmail.com>
2016-03-31 15:43:36 +0200

    Merge layout + merge controller ( todo)

---
+++ src/com/frogans/designer/view/PropertiesLayout/MergeLayoutController.java
@@ -0,0 +1,13 @@
+package com.frogans.designer.view.PropertiesLayout;
+
+import javafx.fxml.FXML;
+
+/**
+ * Created by ABDELKABIR on 31/03/2016.
+ */
+public class MergeLayoutController {
+    @FXML
+    public void initialize(){
+
+    }
+}
