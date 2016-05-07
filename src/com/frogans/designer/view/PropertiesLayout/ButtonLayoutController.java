55edb943f6b2acc0501b3199777c030893de8a6e
Alaeddine <assad.leo007@gmail.com>
2016-05-07 18:50:30 +0200

    buttonLayout Controller

--- src/com/frogans/designer/view/PropertiesLayout/ButtonLayoutController.java
+++ src/com/frogans/designer/view/PropertiesLayout/ButtonLayoutController.java
@@ -1,18 +1,57 @@
 package com.frogans.designer.view.PropertiesLayout;
 
+import com.frogans.designer.FrogansApp;
+import com.frogans.designer.model.Elements.ButtonFSDL;
+import javafx.beans.property.StringProperty;
 import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
 
-/**
- * Created by naouf on 3/31/2016.
- */
 public class ButtonLayoutController {
 
+    ButtonFSDL ButtonFSDL = new ButtonFSDL();
+    private FrogansApp frogansApp;
+    @FXML
+    private TextField idButton;
+    @FXML
+    private ChoiceBox<String> typeChoice;
+    @FXML
+    private TextField filerefText;
+    @FXML
+    private TextField addressText;
+    @FXML
+    private TextField urlText;
+    @FXML
+    private TextField emailText;
 
+
+    public void setFrogansApp(FrogansApp frogansApp) {
+        this.frogansApp = frogansApp;
+    }
 
     @FXML
     public void initialize() {
-
+        fillType();
     }
 
+    private void fillType() {
+        for (StringProperty stringProperty : ButtonFSDL.getGo_toArray()) {
+            typeChoice.getItems().add(stringProperty.get());
+        }
+    }
+    public void fillButtonLayout(String buttonid,
+                                 String fileref,
+                                 String go_to,
+                                 String address,
+                                 String url,
+                                 String email
+    ) {
+        idButton.setText(buttonid);
+        typeChoice.setValue(go_to);
+        filerefText.setText(fileref);
+        addressText.setText(address);
+        urlText.setText(url);
+        emailText.setText(email);
 
+    }
 }
